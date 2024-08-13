a67aae7e769a74b938957b58231e93e9e005343f
Khrystyna Landvytovych <crofty.land@gmail.com>
2024-08-13 14:57:48 +0300

    feat: init docker for backend

---
+++ backend/entrypoint.sh
@@ -0,0 +1,12 @@
+# Wait for Postgres to be ready
+/wait-for-postgres.sh postgresdb
+
+# Perform database migration
+python manage.py makemigrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start Django application with Gunicorn
+gunicorn backend.wsgi:application --bind 0.0.0.0:8000
